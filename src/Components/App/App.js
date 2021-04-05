0f1cc5c5f630d30ce40e1c54df135ffe04caa6a9
Vendy Prum <vendy@thinkful.com>
2021-04-05 16:48:45 -0700

    first commit basic start of a social networking features

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import Card from "./../Card/Card";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,21 @@
+import './Card.css';
+function Card() {
+  return (
+    <div className="main-card-container">
+   
+      <div className="card-content">
+        <div>
+          <img
+            src="./../../img/tea.jpeg"
+            alt="brown tea pot with a clear glass tea cup filled with black tea"
+          />
+        </div>
+        <div>
+          <p>The beautiful teaset</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
